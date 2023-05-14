19967eb823c3b0888ab4936f34e76a81a98c6602
m4rt1nch0 <tinchocometta@hotmail.com>
2023-05-14 18:25:32 -0300

    lista bd

--- orders.js
+++ orders.js
@@ -1,100 +1,132 @@
 const express = require("express");
+require("dotenv").config();
 const bodyParser = require("body-parser");
+const { MongoClient, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://mugiwaras:${process.env.PASSWORD}@test.lyratzj.mongodb.net/test?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 app.use(bodyParser.json());
 
-const orderStates = {
-  CREATED: "CREATED",
-  APPROVED: "APPROVED",
-  NONAPPROVED: "NONAPPROVED",
-};
-
-const orders = [];
-
 function generateOrderId() {
   return Date.now().toString();
 }
-// Ruta para crear una nueva orden
-app.post("/orders", (req, res) => {
-  const {
-    date,
-    amount,
-    articleCode,
-    quantity,
-    discountPercentage,
-    orderTotal,
-    idUser,
-    creditCardType,
-    coin,
-  } = req.body;
 
-  // Genera un nuevo ID para la orden
-  const orderId = generateOrderId();
+// Endpoint para crear una nueva orden
+app.post("/orders", async (req, res) => {
+  try {
+    await client.connect();
+    console.log("Conectado exitosamente a la base de datos");
 
-  // Crea el objeto de la orden
-  const order = {
-    //order es la primary key
-    order: orderId,
-    state: orderStates.CREATED,
-    date: date,
-    amount: amount,
-    articleCode: articleCode,
-    quantity: quantity,
-    discountPercentage: discountPercentage,
-    orderTotal: orderTotal,
-    idUser: idUser,
-    creditCardType: creditCardType,
-    coin: coin,
-  };
+    const ordersCollection = client.db("test").collection("orders");
 
-  // Agrega la orden al arreglo
-  orders.push(order);
+    const {
+      date,
+      amount,
+      articleCode,
+      quantity,
+      discountPercentage,
+      orderTotal,
+      idUser,
+      creditCardType,
+      coin,
+    } = req.body;
 
-  res.json(order);
+    const order = {
+      order: generateOrderId(),
+      state: "CREATED",
+      date: date,
+      amount: amount,
+      articleCode: articleCode,
+      quantity: quantity,
+      discountPercentage: discountPercentage,
+      orderTotal: orderTotal,
+      idUser: idUser,
+      creditCardType: creditCardType,
+      coin: coin,
+    };
+
+    await ordersCollection.insertOne(order);
+    res.json(order);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Error al crear la orden" });
+  } finally {
+    await client.close();
+  }
 });
 
-// Ruta para obtener una orden por su ID
-app.get("/orders/:orderId", (req, res) => {
-  const { orderId } = req.params;
+// Endpoint para obtener una orden por su ID
+app.get("/orders/:orderId", async (req, res) => {
+  try {
+    await client.connect();
+    console.log("Conectado exitosamente a la base de datos");
 
-  // Busca la orden por su ID
-  const order = orders.find((o) => o.order === orderId);
+    const ordersCollection = client.db("test").collection("orders");
 
-  if (!order) {
-    res.status(404).json({ error: "Orden no encontrada" });
-  } else {
-    res.json(order);
+    const { orderId } = req.params;
+
+    const order = await ordersCollection.findOne({ order: orderId });
+
+    if (!order) {
+      res.status(404).json({ error: "Orden no encontrada" });
+    } else {
+      res.json(order);
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Error al obtener la orden" });
+  } finally {
+    await client.close();
   }
 });
 
 //Authorized ->  APPROVED; Rejected -> NONAPPROVED
-app.put("/orders/:orderId", (req, res) => {
+app.put("/orders/:orderId", async (req, res) => {
   const { orderId } = req.params;
   const { state } = req.body;
 
-  // Busca la orden por su ID
-  const index = orders.findIndex((o) => o.id === orderId);
-  const order = orders[index];
-  orders[index] = orders[orders.length - 1];
-  orders.pop();
+  // Conectamos con la base de datos
+  const client = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
-  if (!order) {
-    res.status(404).json({ error: "Orden no encontrada" });
-  } else {
+  try {
+    // Obtenemos la referencia a la colección
+    const collection = client.db("test").collection("orders");
 
-    if(state.toLowerCase() != "authorized" &&  state.toLowerCase() != "rejected"){
-      res.status(400).json({ error: "El estado no es valido." });
-      return
+    // Buscamos la orden por su ID
+    const order = await collection.findOne({ order: orderId });
+
+    if (!order) {
+      res.status(404).json({ error: "Orden no encontrada" });
+      return;
     }
+
+    if (state.toLowerCase() != "authorized" && state.toLowerCase() != "rejected") {
+      res.status(400).json({ error: "El estado no es válido." });
+      return;
+    }
+
     if (state.toLowerCase() == "authorized") {
-      order.state = orderStates.APPROVED;
+      order.state = "APPROVED";
     }
+
     if (state.toLowerCase() == "rejected") {
-      order.state = orderStates.NONAPPROVED;
-    } 
-    orders.push(order);
+      order.state = "NONAPPROVED";
+    }
+
+    // Actualizamos la orden en la base de datos
+    await collection.updateOne({ order: orderId }, { $set: order });
+
     res.json(order);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Error en el servidor" });
+  } finally {
+    // Cerramos la conexión con la base de datos
+    client.close();
   }
 });
 
